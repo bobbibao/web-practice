71a53b000dc62f071ed11ea6804b719e825ddcd7
bobbibao <lehoangbao1125@gmail.com>
2023-03-26 09:59:15 +0700

    Add exercise3.html, .css and demo exercise3.png - prompt, calculate total two num

---
+++ week6-7_javascript/javascript/exercise3.js
@@ -0,0 +1,4 @@
+var num1 = prompt("Enter the first number: ", 0)
+var num2 = prompt("Enter the second number: ", 0)
+var total = parseInt(num1)  + parseInt(num2)
+document.write(num1 + " added to " + num2 + " equas " + total)

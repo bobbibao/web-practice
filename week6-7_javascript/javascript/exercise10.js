042adaff716ced87f2a099ffd1628a76e7d6e74f
bobbibao <lehoangbao1125@gmail.com>
2023-04-03 22:11:51 +0700

    Add exercise10.html, .js and  demo exercise10.png - fix error of check_age function

---
+++ week6-7_javascript/javascript/exercise10.js
@@ -0,0 +1,8 @@
+var age = prompt("Please enter your age: ");
+if (age == 0) {
+    alert("So you're a baby!!!");
+} else if (age < 0 | age > 150) 
+    alert("I think you may be lying about your age");
+else{
+    alert("That's a good age")
+}
